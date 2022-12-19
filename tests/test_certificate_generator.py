5337e98cc7163c986f03a9a946f2fb8cfa7da004
Mateus Muller <mateusmuller@github.com>
2022-12-19 17:33:16 +0000

    test: testing certificate generation

--- tests/test_certificate_generator.py
+++ tests/test_certificate_generator.py
@@ -1,4 +1,32 @@
+import certgenerator
+import pytest
+import os
+
+
 class TestCertificateGenerator():
 
-    def test_dummy(self):
+    @pytest.fixture()
+    def client(self):
+        os.environ["SMTP_EMAIL"] = "demo@demo.com"
+        os.environ["SMTP_PASSWORD"] = "pass"
+        os.environ["SMTP_SERVER"] = "smtp.demo.com"
+        os.environ["SMTP_PORT"] = "589"
+
+        yield certgenerator.CertificateGenerator(
+            '[WORKSHOP FEEVALE] - Certificado de Conclusão',
+            'participantes.csv',
+            'certificate_template.png',
+            640,
+            1000,
+            'DejaVuSans.ttf',
+            200
+        )
+
+    def test_certificate_generate(self, client):
+        filename = client.generate_certificate("NAme HeRE Test")
+        assert filename == "name_here_test.png"
+        assert os.path.exists(filename)
+        os.remove(filename)
+
+    def test_send_email(self, client):
         pass
